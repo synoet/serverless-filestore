75a91769a184ec4878067fdffe13c165090e705f
nysteo <teonys@nyu.edu>
2021-06-05 19:32:59 -0400

    working binary

--- index.ts
+++ index.ts
@@ -3,7 +3,14 @@ import {uploadHttpHandler} from './src/http';
 
 export const upload: APIGatewayProxyHandlerV2 = async(event, context, callback) => {
   await uploadHttpHandler(event, context, callback);
-  return { statusCode: 200, body: ''};
+  return { 
+    statusCode: 200,
+    headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    }, 
+    body: ''
+  };
 }
 
 export const get = () => {
