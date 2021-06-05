8b27bdb61b1c85852b7d123774013880af6fab0e
nysteo <teonys@nyu.edu>
2021-06-05 18:23:01 -0400

    setup function serverless config

--- node_modules/@middy/http-json-body-parser/index.d.ts
+++
@@ -1,9 +0,0 @@
-import middy from '@middy/core'
-
-interface IJsonBodyParserOptions {
-  reviver?: (key: string, value: any) => any
-}
-
-declare const jsonBodyParser : middy.Middleware<IJsonBodyParserOptions, any, any>
-
-export default jsonBodyParser
